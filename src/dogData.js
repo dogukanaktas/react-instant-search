1763d4148ad50232d2f8420540aa82b04908f7aa
Dogukan Aktas <aktasdogukann@gmail.com>
2020-06-25 01:27:03 +0300

    Add dog names and breeds

--- src/dogData.js
+++ src/dogData.js
@@ -1,26 +1,155 @@
- const dog = [
-    {
-      name: "Max",
-      breed: "Labrador"
-    },
-    {
-      name: "Sparky",
-      breed: "German Shephard"
-    },
-    {
-      name: "Rex",
-      breed: "Boxer"
-    },
-    {
-      name: "Sally",
-      breed: "Poodle"
-    },
-    {
-      name: "George",
-      breed: "Pitbull"
-    },
-    {
-      name: "George",
-      breed: "Pitbull"
-    },
-  ]
+const dogData = [
+  {
+    id: 1,
+    name: "Max",
+    breed: "Labrador",
+  },
+  {
+    id: 2,
+    name: "Sparky",
+    breed: "German Shephard",
+  },
+  {
+    id: 3,
+    name: "Rex",
+    breed: "Boxer",
+  },
+  {
+    id: 4,
+    name: "Sally",
+    breed: "Poodle",
+  },
+  {
+    id: 5,
+    name: "George",
+    breed: "Pitbull",
+  },
+  {
+    id: 6,
+    name: "Rocky",
+    breed: "Golden",
+  },
+  {
+    id: 7,
+    name: "Jack",
+    breed: "Rottweiler",
+  },
+  {
+    id: 8,
+    name: "Duke",
+    breed: "Dachshunds",
+  },
+  {
+    id: 9,
+    name: "Bailey",
+    breed: "Havanese",
+  },
+  {
+    id: 10,
+    name: "Bella",
+    breed: "Boxer",
+  },
+  {
+    id: 11,
+    name: "Buddy",
+    breed: "Pomeranian",
+  },
+  {
+    id: 12,
+    name: "Lucky",
+    breed: "Yorkshire Terrier",
+  },
+  {
+    id: 13,
+    name: "Harley",
+    breed: "Pug",
+  },
+  {
+    id: 14,
+    name: "Lily",
+    breed: "Cane Corso",
+  },
+  {
+    id: 15,
+    name: "Sadie",
+    breed: "Cocker",
+  },
+  {
+    id: 16,
+    name: "Coco",
+    breed: "Shih Tzu",
+  },
+  {
+    id: 17,
+    name: "Maggie",
+    breed: "Doberman",
+  },
+  {
+    id: 18,
+    name: "Molly",
+    breed: "Barnese Mountain Dogs",
+  },
+  {
+    id: 19,
+    name: "Toby",
+    breed: "Mastiff",
+  },
+  {
+    id: 20,
+    name: "Lola",
+    breed: "Chihuahua",
+  },
+  {
+    id: 21,
+    name: "Angel",
+    breed: "English Springer",
+  },
+  {
+    id: 22,
+    name: "Princess",
+    breed: "Vizslas",
+  },
+  {
+    id: 23,
+    name: "Emma",
+    breed: "Maltese",
+  },
+  {
+    id: 24,
+    name: "Buster",
+    breed: "Weimaraner",
+  },
+  {
+    id: 25,
+    name: "Oscar",
+    breed: "Collie",
+  },
+  {
+    id: 26,
+    name: "Oliver",
+    breed: "Basset Hound",
+  },
+  {
+    id: 27,
+    name: "Winston",
+    breed: "Belgian Malinois",
+  },
+  {
+    id: 28,
+    name: "Zoe",
+    breed: "Samoyed",
+  },
+  {
+    id: 29,
+    name: "George",
+    breed: "Bloodhound",
+  },
+  {
+    id: 30,
+    name: "Hai",
+    breed: "Pekingese",
+  },
+  
+]
+
+export default dogData
