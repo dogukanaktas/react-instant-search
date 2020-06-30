b80f07dbbb37e7e2961b3a4c57bcc6d6a4005e28
Dogukan Aktas <aktasdogukann@gmail.com>
2020-06-30 21:29:34 +0300

    Move grid styles to index.css

--- src/DogList.js
+++ src/DogList.js
@@ -1,22 +1,13 @@
 import React from "react";
 import Dog from "./Dog";
+import "./index.css";
 
 const DogList = (props) => {
   const dogs = props.filteredDogs.map((item) => (
     <Dog key={item.id} name={item.name} breed={item.breed} />
   ));
 
-  const list = {
-    display: "grid",
-    gridTemplateColumns: 'repeat(6, auto)',
-    gridGap: '1em',
-  };
-
-  return (
-    <div style={list} className="list">
-      {dogs}
-    </div>
-  )
+  return <div className="list">{dogs}</div>;
 };
 
 export default DogList;
